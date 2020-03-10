5977f4fbfdc390d4fc5bb3a869fe93bc0a5d5f68
MrGoatMan <lukenewman@codelab-18.rockhurst.int>
2020-03-10 08:49:45 -0500

    whick

---
+++ venture.py
@@ -0,0 +1,10 @@
+from blaster import Blaster
+from binaryQuest import Binary
+
+print("Howdy there, captain! My name is SCOR, the mechanical navigator of the SS...")
+shipName = input("Huh, that's weird... what was the ship's name again? ")      
+print(f"Ah, that's right. The mighty {shipName}. Beautiful birdie, isn't she?")
+beautyBird = " "
+Binary(beautyBird)
+
+
