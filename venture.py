2027a0fdf4a5da8e0169bb510dce9c241f1f27c6
MrGoatMan <LukeNewman@amdg.rockhursths.edu>
2020-03-12 09:26:42 -0500

    Whack

--- venture.py
+++ venture.py
@@ -4,7 +4,13 @@ from binaryQuest import Binary
 print("Howdy there, captain! My name is SCOR, the mechanical navigator of the SS...")
 shipName = input("Huh, that's weird... what was the ship's name again? ")      
 print(f"Ah, that's right. The mighty {shipName}. Beautiful birdie, isn't she?")
-beautyBird = " "
-Binary(beautyBird)
+
+beautyBird = Binary()
+if (beautyBird.which == True):
+    print("Truly, truly a work of art. Good taste, mate.")
+if (beautyBird.which == False):
+    print("Bit rude, ain't ya?")
+capName = input("What're you called, anywho? ")
+print(f"Aye, aye, Captain {capName}! Welcome to the crew, mate.")
 
 
